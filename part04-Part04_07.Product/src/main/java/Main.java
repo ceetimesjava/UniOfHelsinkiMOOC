6ab0751b8d83030ee198c5021685dcaff8fbb809
Habeeb Mustapha <haademustapha@gmail.com>
2022-06-05 22:59:35 -0500

    third commit

--- part04-Part04_07.Product/src/main/java/Main.java
+++ part04-Part04_07.Product/src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main {
         // This is an empty main method for testing the Product class
         // Try:
 
-        // Product product = new Product("Banana", 1.1, 13);
-        // product.printProduct();
+        Product product = new Product("Banana" , 1.1, 13);
+        
+        product.printProduct();
     }
 }
